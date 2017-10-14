a2e2146a4929766e1adfb1d1cfaada7055acb21e
mecha <v3suv1us@users.noreply.github.com>
2017-10-13 21:36:25 -0700

    uncompressed translation function, encoder vars, basic functions
    also added zeropowerbehavior

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HWRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HWRobot.java
@@ -18,6 +18,13 @@ public class HWRobot
     public double powBL = 0;
     public double powBR = 0;
 
+
+    static final double     COUNTS_PER_MOTOR_REV    = 1120 ;
+    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415) ;
+    static final double     DISTANCE_MODIFIER       = 1.414 ;
+
     /* local OpMode members and objects */
     HardwareMap hwMap           =  null;
 
@@ -38,6 +45,12 @@ public class HWRobot
         mtrFR.setDirection(DcMotor.Direction.FORWARD);
         mtrBL.setDirection(DcMotor.Direction.REVERSE);
         mtrBR.setDirection(DcMotor.Direction.FORWARD);
+
+        //zero power behavior
+        mtrFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        mtrFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        mtrBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        mtrBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // Set power for all motors.
         mtrFL.setPower(powFL);
@@ -62,9 +75,67 @@ public class HWRobot
         mtrBL.setMode(mode);
         mtrBR.setMode(mode);
     }
+    public void mtrSetSpeed(double speed){
+        mtrFL.setPower(speed);
+        mtrFR.setPower(speed);
+        mtrBL.setPower(speed);
+        mtrBR.setPower(speed);
+    }
 
-    //Function to translate on the field (encoders)
-    public void translate(){}
+    //Function to translate on the field (encoders)- currently reconstructed
+    //TODO make repitition into own functions
+    public void translate(String dir, double speed, double inches, boolean active){
+        double inchLocal;
+        String dirLocal;
+        int posFL, posFR, posBL, posBR;
+        int a = 1;
+        int b = 1;
+
+        dirLocal = dir.toLowerCase();
+        inchLocal = inches * DISTANCE_MODIFIER;
+
+        if(dir == "fwd" || dir == "forward") {
+            a = 1;
+            b = 1;
+        }
+        else if(dir == "bk" || dir == "backwards") {
+            a = -1;
+            b = -1;
+        }
+        else if(dir == "left") {
+            a = -1;
+            b = 1;
+        }
+        else if (dir == "right") {
+            a = 1;
+            b = -1;
+        }
+
+        int countTarget;
+
+        if(active) {
+            countTarget = mtrFL.getCurrentPosition() + (int) (inches * COUNTS_PER_INCH);
+            posFL = a * countTarget;
+            posFR = b * countTarget;
+            posBL = b * countTarget;
+            posBR = a * countTarget;
+
+            mtrFL.setTargetPosition(posFL);
+            mtrFR.setTargetPosition(posFR);
+            mtrBL.setTargetPosition(posBL);
+            mtrBR.setTargetPosition(posBR);
+
+            mtrChangeMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            mtrSetSpeed(speed);
+
+            mtrSetSpeed(0);
+
+            mtrChangeMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+        
+    }
+
 
     //Function to rotate on the field (encoders)
     public void rotate(){}
