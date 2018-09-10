86ae85284ead82e9f79b2b89c77805d990c96cc4
aadhi <byeman20@gmail.com>
2018-09-09 17:32:49 -0700

    new extension op mode created

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/called/TestExtension.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.called;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by aadhidhya on 9/9/18.
+ */
+
+public class TestExtension extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        //to be overriden when extended for actual op mode
+    }
+    public void translate() {
+
+
+    }
+    public void inits() {
+
+    }
+    public void rotate() {
+
+    }
+    //cv functions
+}
+
