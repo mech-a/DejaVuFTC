0af6a0147423920cb0c90a40fcf6146f984abe6c
MeowOfDoom720 <32623092+MeowOfDoom720@users.noreply.github.com>
2019-03-02 13:16:56 -0800

    1 inch farther

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/CraterNoRedundancy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/CraterNoRedundancy.java
@@ -17,6 +17,9 @@ public class CraterNoRedundancy extends LinearOpMode {
     private double speed = 0.16;
     private int time = 50;
 
+    private double inchesFromHookRightToWall = 39;
+    private double rotationToParallel = 126;
+
     @Override public void runOpMode() {
         r.start(hardwareMap, telemetry);
         r.init();
@@ -29,6 +32,8 @@ public class CraterNoRedundancy extends LinearOpMode {
 
         waitForStart();
 
+        r.positionDrive(2, 100,0.75);
+
         //detach from the lander
 
         r.armMotors[0].setPower(-1);
@@ -40,7 +45,7 @@ public class CraterNoRedundancy extends LinearOpMode {
         //r.positionDrive(0,840,0.3 );
 
         r.armMotors[0].setPower(0.5);
-        while(!isStopRequested() && r.armMotors[0].getCurrentPosition() <= 782) {}
+        while(!isStopRequested() && r.armMotors[0].getCurrentPosition() <= 777) {}
         r.armMotors[0].setPower(0);
 
         sleep(100);
@@ -74,22 +79,22 @@ public class CraterNoRedundancy extends LinearOpMode {
             r.translate(Enums.Direction.RIGHT, 8, speed);
         }
 
-        r.translate(Enums.Direction.BACK, 7, speed);
-        r.translate(Enums.Direction.FWD, 7, speed);
+        r.translate(Enums.Direction.BACK, 10, speed);
+        r.translate(Enums.Direction.FWD, 10, speed);
 
         if(x==GoldPosition.MIDDLE) {
-            r.translate(Enums.Direction.RIGHT,39+7, speed);
+            r.translate(Enums.Direction.RIGHT,40+7, speed);
         }
         else if (x==GoldPosition.RIGHT) {
-            r.translate(Enums.Direction.RIGHT, 21+39, speed);
+            r.translate(Enums.Direction.RIGHT, 21+40, speed);
         }
         else {
-            r.translate(Enums.Direction.RIGHT, 39-8, speed);
+            r.translate(Enums.Direction.RIGHT, 40-8, speed);
         }
 
         r.rotate("ccw", speed, 130);
         sleep(time);
-        r.translate(Enums.Direction.LEFT, 5, speed);
+        r.translate(Enums.Direction.LEFT, 6, speed);
         sleep(time);
         r.translate(Enums.Direction.BACK, 37, speed);
         sleep(time);
